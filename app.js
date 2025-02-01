7ae4866c770419b1bc799da4a81e222bb869b917
Naqvi <44436701+nrcool@users.noreply.github.com>
2025-02-01 04:07:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-console.log(process.env.API_KEY);
+
 app.get("/", (req, res) => {
   res.sendFile("./index.html", { root: "." });
 });
